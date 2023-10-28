92998e204fbdb2cdcb2dd20f8a38faa486d79a1a
FahrianAfdholi <16521492@mahasiswa.itb.ac.id>
2023-10-28 23:04:10 +0700

    add: schema database

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,12 @@ COPY package*.json .
 COPY tsconfig.json ./tsconfig.json
 
 RUN npm install
+RUN npm install prisma typescript ts-node @types/node --save-dev
 
 COPY . .
 
+RUN npx prisma migrate dev --name init
+RUN npm install @prisma/client
 RUN npm run build
 
 CMD ["npm", "start"]
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import crypto from "crypto";
 
 dotenv.config();
 
@@ -7,12 +8,22 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.use((req,res,next)=>{
-    console.log(`🤖 Nutricraft Logging 🤖  ${req.ip} : \x1b[1m${req.method}\x1b[0m ${req.originalUrl} || :${req.statusCode}:`)
+    console.log(`🤖 Nutricraft Logging 🤖  ${req.ip} : \x1b[1m${req.method}\x1b[0m ${req.originalUrl} || :${res.statusCode}:`)
     next();
 })
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    const header = {alg:"HS256", typ:"JWT"}
+    const payload = {id:1, email:"kontol@gmail.com"}
+
+    const encodeHeader = Buffer.from(JSON.stringify(header)).toString("base64")
+    const encodePayload = Buffer.from(JSON.stringify(payload)).toString("base64")
+
+    const signature = crypto
+        .createHmac("sha256","kontol-kuda")
+        .update(encodeHeader+"."+encodePayload)
+        .digest("base64")
+    res.send(`${encodeHeader} : ${encodePayload} : ${signature}`);
 });
 
 app.listen(port, () => {
