38f9bd2aee312c38c662eb331e4944d50ddc97b4
Fahrian Afdholi <13521031@std.stei.itb.ac.id>
2023-11-07 10:54:36 +0700

    update: Forum

--- router/router.ts
+++ router/router.ts
@@ -2,9 +2,12 @@ import {Express} from "express";
 import AuthRouter from "./auth/auth.router";
 import ContentRouter from "./content/content.router";
 import ForumRouter from "./forum/forum.router";
+import LikeForumRouter from "./forum/like.forum.router";
 
 export default function MainRouter(router: Express) {
     AuthRouter(router)
     ContentRouter(router)
     ForumRouter(router)
+    ContentRouter(router)
+    LikeForumRouter(router)
 }
