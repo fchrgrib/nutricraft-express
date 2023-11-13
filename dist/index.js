072f0df607909bba5dd865ee6014c38755ce9ac1
Fahrian Afdholi <13521031@std.stei.itb.ac.id>
2023-11-14 06:08:56 +0700

    add: file request

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,28 +7,16 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const router_1 = __importDefault(require("./router/router"));
-const level_soap_service_1 = require("./soap/service/level.soap.service");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
-const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
 app.use(express_1.default.json());
+app.use(bodyParser());
 app.use((0, cookie_parser_1.default)());
-app.use(fileUpload());
+app.use(express_1.default.static('image'));
 dotenv_1.default.config();
-app.use((req, res, next) => {
-    console.log(`🤖 Nutricraft Logging 🤖  ${req.ip} : \x1b[1m${req.method}\x1b[0m ${req.originalUrl} || :${res.statusCode}:`);
-    next();
-});
 (0, router_1.default)(app);
-app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    if (yield (0, level_soap_service_1.NewLevelAccount)('kontol kuda')) {
-        res.status(200).send({ status: "ok" });
-    }
-    else {
-        res.status(400).send({ status: "bau" });
-    }
-}));
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
